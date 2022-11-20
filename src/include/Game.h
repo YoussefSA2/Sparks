b6d3203c6dd9822b6e3a38120092866f3392cd70
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2022-11-20 17:41:52 +0100

    refactor: move all constants to a Constants.h file

--- src/include/Game.h
+++ src/include/Game.h
@@ -1,36 +1,25 @@
 #ifndef _GAME_H
 #define _GAME_H
 
+#include "./Constants.h"
 #include "./Player.h"
+#include "./Map.h"
 #ifdef _WIN32
     #include <conio.h>
 #endif
 
 /*
-* Game states
-*/
-#define GAME_IS_FINISHED 99
-#define GAME_IS_RUNNING 0
-
-/*
-* Player inputs
+* Boolean values
 */
 
-#define EXIT_INPUT 'q'
-// Direction inputs
-#define MOVE_NORTH_INPUT '8'
-#define MOVE_NORTH_EAST_INPUT '9'
-#define MOVE_EAST_INPUT '6'
-#define MOVE_SOUTH_EAST_INPUT '3'
-#define MOVE_SOUTH_INPUT '2'
-#define MOVE_SOUTH_WEST_INPUT '1'
-#define MOVE_WEST_INPUT '4'
-#define MOVE_NORTH_WEST_INPUT '7'
+#define true 1
+#define false 0
 
 int saveGame();
 void clearScreen();
 char getPlayerInput();
 int handlePlayerInput(char input, Player* player);
+char handlePlayerMove(Player* player, char direction);
 void printLastAction(char gameState);
 
 #endif
--- src/include/Map.h
+++ src/include/Map.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
+#include "./Constants.h"
 #include "./Player.h"
 
 
@@ -19,7 +20,8 @@
 #endif
 
 
-int **generateMap(int MAP_SIZE);
-void showMap(int** map, int map_size, Player player);   
+int **generateMap(int mapSize);
+char getOppositeDirection(char direction);
+void showMap(int** map, int mapSize, Player player);   
 
 #endif
