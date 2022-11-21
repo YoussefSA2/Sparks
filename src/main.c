4adfa7ffb9e86763fe83a960ac5960862df0a191
YoussefSA2 <117744986+YoussefSA2@users.noreply.github.com>
2022-11-21 16:56:03 +0100

    Update main.c
    (cherry picked from commit 2ac64f762e78d445b6f0480334c65ec317d36118)

--- src/main.c
+++ src/main.c
@@ -23,10 +23,19 @@ int main(void)
   
     while(gameState != GAME_IS_FINISHED)
     {   
-        gameState = handlePlayerInput(getPlayerInput(), &player, map);
-        clearScreen();
-        showMap(map, MAP_SIZE, player);
-        printLastAction(gameState);
+        #ifdef _WIN32 // Windows
+            if (kbhit()) {
+                gameState = handlePlayerInput(getPlayerInput(), &player, map);
+                clearScreen();
+                showMap(map, MAP_SIZE, player);
+                printLastAction(gameState);
+            }
+        #else
+            gameState = handlePlayerInput(getPlayerInput(), &player, map);
+            clearScreen();
+            showMap(map, MAP_SIZE, player);
+            printLastAction(gameState);
+        #endif
     }
 
     clearScreen();
