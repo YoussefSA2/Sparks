a00f221ad67c2144c6798bcdab19f59dba3fa7e8
Sithursha <46sithursha@gmail.com>
2022-11-21 20:54:08 +0100

    add show coordinates

--- src/Coordinates.c
+++ src/Coordinates.c
@@ -1,1 +1,12 @@
-#include "./include/Coordinates.h"
+#include <stdio.h>
+
+typedef struct Coordinates Coordinates;
+struct Coordinates{
+    int x;
+    int y;
+};
+
+void showCoordinates(Coordinates position){
+    printf("(x,y): %d %d", position.x, position.y);
+
+}
---
+++ src/include/Coordinate.h
@@ -0,0 +1,8 @@
+typedef struct Coordinates Coordinates;
+struct Coordinates{
+    int x;
+    int y;
+
+};
+
+void showCoordinates(Coordinates coordinates);
