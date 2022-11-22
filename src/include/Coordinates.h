559bea79ebaa32633ac43725be10a403508d703d
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2022-11-22 07:09:10 +0100

    Update Coordinates.h

--- src/include/Coordinates.h
+++ src/include/Coordinates.h
@@ -1,5 +1,13 @@
+#ifndef _COORDINATES_H
+#define _COORDINATES_H
+
 typedef struct Coordinates Coordinates;
 struct Coordinates{
     int x;
     int y;
+
 };
+
+void showCoordinates(Coordinates coordinates);
+
+#endif
