9f1509944ef5708872c22b55b31749237f566a54
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2022-11-22 07:56:24 +0100

    change coordinates formatting

--- src/Coordinates.c
+++ src/Coordinates.c
@@ -1,6 +1,6 @@
 #include "./include/Coordinates.h"
 
 void showCoordinates(Coordinates position){
-    printf("(x,y): %d %d", position.x, position.y);
+    printf("(%d, %d)", position.x, position.y);
 
 }
