29789217c50f008c484ee13ab6d9b2f820d03089
Sithursha <46sithursha@gmail.com>
2022-12-03 10:16:44 +0100

    add-player-condition-lost-in-checkGameStates

--- src/Game.c
+++ src/Game.c
@@ -136,9 +136,15 @@ int checkGameState(Player player, int lastPlayerAction)
     int playerWon = player.position.x == MAP_SIZE-1
      && player.position.y == MAP_SIZE-1;
 
+    int playerLost = player.energy <= 0;
+    
     if (playerWon)
     {
         return PLAYER_WON;
+    }
+    else if (playerLost)
+    {
+        return PLAYER_LOST;
     }
     else
     {
