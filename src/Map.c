b63ad5d9893bee24d0f75c84909a56250660fb86
Charles-Meldhine Madi Mnemoi <deanbah01@gmail.com>
2022-12-12 12:50:02 +0100

    Chose the difficulty of the laby
    (cherry picked from commit 7807ecb8dba261629e4878dd72b1aab86165787f)

--- src/Map.c
+++ src/Map.c
@@ -9,6 +9,9 @@
 */
 int **generateMap(int mapSize){
     int** map = NULL;
+    int nbEmptySquare,nbObstacle;
+    int choix;
+    double FoodRatio,ObstacleRatio;
 
     map=malloc(mapSize * sizeof(int*));
 
@@ -21,16 +24,62 @@ int **generateMap(int mapSize){
     {
         for(int j=0 ; j<mapSize; j++)
         {
-            map[i][j]=rand()%3;                                    
-            map[0][0]=TREE;                                            
-                                                                
-            map[mapSize-1][mapSize-1]=EXIT;
-        }
+            map[i][j]=rand()%2;
+            if (map[i][j]==0)
+            { 
+                nbEmptySquare++;
+            }
+            else
+            {
+                nbObstacle++;
+            }
+        } 
+    } 
+    printf("Choisissez la difficulté du labyrinte :\n");
+    printf("1. Facile (peu d'obstacles)\n");
+    printf("2. Moyen (un nombre moyen d'obstacles)\n");
+    printf("3. Difficile (beaucoup d'obstacles)\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+    if (choix == 1)
+    {
+    FoodRatio = 0.7;
+    ObstacleRatio = 1.4;
+    }
+    else if (choix == 2)
+    {
+    FoodRatio = 0.7;
+    ObstacleRatio = 0.4;
+    }
+    else 
+    {
+    FoodRatio = 0.4;
+    ObstacleRatio = 0.1;
     }
 
-    return map;
-}
+    int nbBonus=nbEmptySquare*FoodRatio; 
+    int nbMalus=nbObstacle*ObstacleRatio;
+    while(nbBonus>0 || nbMalus>0)
+    {
+        int line = rand() % mapSize;
+        int column= rand() % mapSize;
 
+        if ((map[line][column]==TREE))
+        {
+            map[line][column]=FOOD;
+            nbBonus--;
+        }
+        else
+        {
+            map[line][column]=TREE;
+            nbMalus--;
+        }
+    } 
+    map[0][0]=TREE; 
+    map[mapSize-1][mapSize-1]=EXIT;
+    
+return map;
+}
 /*
 * Displays the map.
 */
@@ -40,34 +89,34 @@ void showMap(int **map, int mapSize, Player player)
     {   
         for(int j=0;j < mapSize; ++j)
         {
-            //show Player
             if (i == player.position.y && j == player.position.x)
             {
                 printf(" %s ", PLAYER_EMOJI);
             }
-            else if(map[i][j] == TREE)
+            else
             {
-                printf(" %s ", TREE_EMOJI);
+                switch(map[i][j])
+                {
+                    case TREE:
+                    printf(" %s ", TREE_EMOJI);
+                    break;
+                    case OBSTACLE:
+                    printf(" %s ", OBSTACLE_EMOJI);
+                    break;
+                    case FOOD:
+                    printf(" %s ", FOOD_EMOJI);
+                    break;
+                    case EXIT:
+                    printf(" %s ", EXIT_EMOJI);
+                    break;
+                }
             }
-            else if(map[i][j] == OBSTACLE)
-            {
-                printf(" %s ", OBSTACLE_EMOJI);
-            }
-            else if(map[i][j] == FOOD)
-            {
-                printf(" %s ", FOOD_EMOJI);
-            }
-            else if(map[i][j] == EXIT)
-            {
-                printf(" %s ", EXIT_EMOJI);
-            }
-            
         }
         printf("\n");
 
     }
+    free(map);
 }
-
 /*
 * @brief Frees the memory allocated for the map.
 * @param map The map to free.
