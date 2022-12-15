5b7adb2d7832e043874039422c0845fcf757cd5c
Charles-Meldhine Madi Mnemoi <63333367+cmnemoi@users.noreply.github.com>
2022-12-15 14:02:00 +0100

    Save and load previous game on Windows (#81)
    * Save and load previous game PR (#75)
    
    Co-authored-by: Sithursha <46sithursha@gmail.com>
    (cherry picked from commit 4383d99887655ad8d34fb9ea7dcb468731293ebd)
    
    * fix conflict better
    
    * add .exe files to .gitignore

--- src/main.c
+++ src/main.c
@@ -13,24 +13,26 @@
 */
 int main(void)
 {
-    int lastPlayerAction = GAME_IS_RUNNING;
+    showMenu();
+    
     int gameIsFinished = false;
 
     int** map = generateMap(MAP_SIZE);
     Player player = initPlayer();
-
+    
+    do{} while (launchGame(getPlayerInput(), &player, map) == INVALID_LAUNCH_GAME_CHOICE);
+    
     showMap(map, MAP_SIZE, player);
-
   
     while(!gameIsFinished)
     {   
         if (kbhit()) {
-            lastPlayerAction = handlePlayerInput(getPlayerInput(), &player, map);
+            int lastPlayerAction = handlePlayerInput(getPlayerInput(), &player, map);
             clearScreen();
             showMap(map, MAP_SIZE, player);
             printLastAction(lastPlayerAction);
             displayAvailableCommands();
-            gameIsFinished = checkGameState(player, lastPlayerAction);
+            gameIsFinished = checkGameState(player, lastPlayerAction, map);
         }
         
     }
